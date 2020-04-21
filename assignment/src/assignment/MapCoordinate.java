c279c43e3e450d8bb6c16a9f268e6502cd089f0d
Gokul <gokuladapa.com>
2020-04-21 15:16:20 +0530

    first commit

--- assignment/src/assignment/MapCoordinate.java
+++ assignment/src/assignment/MapCoordinate.java
@@ -1,6 +1,6 @@
 package assignment;
 
-public class MapCoordinate {
+public class MapCoordinate implements Comparable<MapCoordinate>{
 
 	private double longitude;
 	private double latitude;
@@ -11,6 +11,52 @@ public class MapCoordinate {
 		this.longitude = longitude;
 		this.latitude = latitude;
 		this.altitude = altitude;
+	}
+
+	@Override
+	public int compareTo(MapCoordinate o) {
+		if(this.longitude > o.longitude & this.latitude > o.latitude & this.altitude > o.altitude)
+		{
+			return 1;
+		}
+		return 0;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(altitude);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(latitude);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(longitude);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		MapCoordinate other = (MapCoordinate) obj;
+		if (Double.doubleToLongBits(altitude) != Double.doubleToLongBits(other.altitude))
+			return false;
+		if (Double.doubleToLongBits(latitude) != Double.doubleToLongBits(other.latitude))
+			return false;
+		if (Double.doubleToLongBits(longitude) != Double.doubleToLongBits(other.longitude))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "MapCoordinate [longitude=" + longitude + ", latitude=" + latitude + ", altitude=" + altitude + "]";
 	}
 
 }
